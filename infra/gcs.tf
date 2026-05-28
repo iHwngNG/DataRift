54e46c6751663489f4be52f7c6b47011eff9d670
iHwngNG <hwngnguyennnn@gmail.com>
2026-05-29 01:02:38 +0700

    feat(infra): grant Compute SA objectAdmin and objectViewer IAM roles on GCS buckets

--- infra/gcs.tf
+++ infra/gcs.tf
@@ -77,3 +77,26 @@ resource "google_storage_bucket" "silver_lakehouse" {
     environment = var.environment
   }
 }
+
+# ------------------------------------------------------------------------------
+# GCS IAM Access Control: Grant Compute Service Account storage object admin access
+# ------------------------------------------------------------------------------
+resource "google_storage_bucket_iam_member" "bronze_raw_writer" {
+  bucket = google_storage_bucket.bronze_raw.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "silver_lakehouse_writer" {
+  bucket = google_storage_bucket.silver_lakehouse.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+# Grant project-level Storage Object Viewer to the Default Compute Service Account
+# This is required for Google Cloud Build to read uploaded source archives from the staging bucket.
+resource "google_project_iam_member" "compute_storage_viewer" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
